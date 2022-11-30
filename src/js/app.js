5a9c2ab8bff34cfd91906e2c6615a30f3c3e759e
Петър Стоев <petarstoev0505@gmail.com>
2022-11-30 17:52:52 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const price = document.querySelector(".price").textContent;
-  // console.log(price);
-  let products = document.querySelector(".product");
-  
+  let products = document.querySelectorAll(".product");
 
-   
-  products.setAttribute("data-price", price);
-  // console.log(products);
+  products.forEach((element) => element.setAttribute("data-price", price));
 
-  
+  console.log(products);
 });
